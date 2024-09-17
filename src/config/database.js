887d11c870684bf84463236c034a5e60c3c29aee
saurabh <ext-saurabh1@suriconsumer.com>
2024-09-18 00:10:34 +0530

    removed log

--- src/config/database.js
+++ src/config/database.js
@@ -2,21 +2,13 @@ const { Pool } = require('pg');
 const neo4j = require('neo4j-driver');
 require('dotenv').config();
 
-let postgresPool;
-
-try {
-   postgresPool = new Pool({
+  const postgresPool = new Pool({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
   });
-  
-  console.log("=======", postgresPool)
-} catch (error) {
-  console.log('-----------', error)
-}
 
 const neo4jDriver = neo4j.driver(
   process.env.NEO4J_URI,
